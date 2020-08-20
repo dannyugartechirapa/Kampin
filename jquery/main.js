4268a60e2b1580f21b9d0698c3da0a4477797fcc
Danny <ugarte1chirapa1danny@gmail.com>
2020-08-20 17:12:20 -0500

    jquery y sus operaciones

---
+++ jquery/main.js
@@ -0,0 +1,18 @@
+var formulario =document.getElementById('formulario');
+
+formulario.addEventListener('submit',function(e){
+e.preventDefault();
+var datos= new FormData(formulario);
+fetch('consultas.php',{
+        method: 'POST',
+        body:   datos
+
+})
+.then( res => res.json())
+.then(  dato =>{
+    console.log(dato);
+        
+})
+
+
+})
